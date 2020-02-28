3325573161e7a5e3d34136b249378813ca52d6c8
Amanda Kuenzle <amandakunzle@gmail.com>
2020-02-28 22:42:35 +0100

    added calculation test

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 import math
+import test_list
+
+# format of input text file: PLZ;ORT;Kanton;Longitude;Latitude
+# 9500;Wil SG;SG;9.043992;47.459015
+
+masterLocation = [
+    {
+    "zip": "9500",
+    "city": "Wil",
+    "canton": "SG",
+    "longitude": 9.043992,
+    "latitude": 47.459015
+    } ]
+
+#print(masterLocation)
+# access key in list
+# print(list[0]["zip"])
 
 # distance between two coordinates:
 # longitude = x, latitude = y
@@ -9,25 +26,10 @@ import math
 # d = math.sqrt(pow(5 - 9, 2) + pow(3 - -4, 2))
 # print(d)
 
-# format of input text file: PLZ;ORT;Kanton;Longitude;Latitude
-# 9500;Wil SG;SG;9.043992;47.459015
+longWil = 9.043992
+latWil = 47.459015
 
-list = [
-    {
-    "zip": "9500",
-    "city": "Wil",
-    "canton": "SG",
-    "longitude": 9.043992,
-    "latitude": 47.459015
-    },
-    {
-    "zip": "2556",
-    "city": "Schwadernau",
-    "canton": "BE",
-    "longitude": 7.314355,
-    "latitude": 47.121929
-    } ]
+for i in range(len(test_list.locations)-1):
+    distance = math.sqrt(pow(longWil - test_list.locations[i]["longitude"], 2) + pow(latWil - test_list.locations[i]["latitude"], 2))
 
-# print(list)
-# print(list[0]["zip"])
-# print(list[0]["city"])
+print(distance)
